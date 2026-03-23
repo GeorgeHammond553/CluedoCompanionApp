748784be18041d927f0e0368ef38adcf09a387cd
GeorgeHammond553 <23662672@stu.mmu.ac.uk>
2026-03-23 19:49:04 +0000

    new modular scene switch added

---
+++ CluedoCompanionAppUP/Assets/Scripts/MainSceneManagerGeorgecopy.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneManagerGeorgecopy : MonoBehaviour
+{
+
+    public void AppTutorial()
+    {
+        SceneManager.LoadScene("App Tutorial");
+    }
+
+    public void CluedoTutorial()
+    {
+        SceneManager.LoadScene("Cluedo Tutorial");
+    }
+
+    public void GameSetup()
+    {
+        SceneManager.LoadScene("Game Setup");
+    }
+
+    public void MainScene()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    public void PlayerSheet()
+    {
+        SceneManager.LoadScene("PlayerSheet");
+    }
+
+    //public void *ENTERSCENE NAME HERE*()         *GEORGE - NO SPACES*
+    //{
+    //    SceneManager.LoadScene("*ENTERSCENE NAME HERE*");      *GEORGE - NO SPACES*
+    //}  *REMEMBER TO ADD NEW SCENES TO THE SCENE LIST IN SETTINGS OR IT WONT WORK* 
+
+}
