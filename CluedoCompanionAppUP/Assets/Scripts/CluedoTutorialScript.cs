0ab4edfa3e3e95de487b253c36483d7b69425624
GeorgeHammond553 <23662672@stu.mmu.ac.uk>
2026-03-23 19:37:22 +0000

    Cluedotutorial script added and new scene version ready

--- CluedoCompanionAppUP/Assets/Scripts/CluedoTutorialScript.cs
+++ CluedoCompanionAppUP/Assets/Scripts/CluedoTutorialScript.cs
@@ -1,16 +1,48 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CluedoTutorialScript : MonoBehaviour
 {
+    public Image displayImage;
+    public Sprite[] pages;
+    private int currentPage = 0;
+    public TMP_Text pageNumberText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ShowPage();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void NextPage()
+    {
+        if (currentPage < pages.Length - 1)
+        {
+            currentPage++;
+            ShowPage();
+        }
+    }
+
+    public void PreviousPage()
+    {
+        if (currentPage > 0)
+        {
+            currentPage--;
+            ShowPage();
+        }
+    }
+
+    void ShowPage()
+    {
+        displayImage.sprite = pages[currentPage];
+        pageNumberText.text = $"{currentPage + 1} / {pages.Length}";
+
     }
 }
